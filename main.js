d0dbd9c63a9b1b41d0e238e9906918c042ef6c87
Somethingextra <somethingextrasas@gmail.com>
2019-11-23 17:44:16 +1000

    Updated
    teee

--- main.js
+++ main.js
@@ -1,17 +1,47 @@
-var money = 0.1;
+var money = 1;
 var intrest = 2;
-var price = 2;
+var price = 1.05;
+var price2 = 1.05;
+var delay = 10;
+var delaymax = 10;
+var speedy = 1000;
 
 function increasemoney() {
     money += (money /= 100) * intrest;
-    document.getElementById("clicks").textContent = "$" + money;
-    document.getElementById("intrest").textContent = "intrest: %" + intrest;
-    document.getElementById("upgradesP").textContent = "price: $" + price;
 }
 function buyupgrade() {
     if(money >= price) {
        intrest += 1;
        money -= price;
-       price *= 5.4; 
+       price *= 1.5; 
     }
 }
+function buyupgrade2() {
+    if(money >= price2) {
+        speedy /= 1.5;
+        money -= price2;
+        price2 *= 5.4; 
+     }
+}
+function updateUI() {
+    document.getElementById("delay").textContent = "intrest pay: " + delay;
+    document.getElementById("clicks").textContent = "$" + money;
+    document.getElementById("intrest").textContent = "intrest: %" + intrest;
+    document.getElementById("upgradesP").textContent = "price: $" + price;
+    document.getElementById("upgradesP2").textContent = "price: $" + price2;
+}
+function update() {
+    function timer() {
+        if (money < 1) {
+            money = 1;
+        }
+        updateUI();
+        delay -= 1;
+        if(delay <= 0) {
+            increasemoney();
+            delay = delaymax;
+        }
+    }
+    setInterval(timer, speedy);
+}
+window.onload = update;
