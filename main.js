0e1bcbc910854c21f275e09648a53b7a7ebeb7eb
Somethingextra <somethingextrasas@gmail.com>
2020-01-29 09:35:35 +1000

    hmmmm

--- main.js
+++ main.js
@@ -79,4 +79,19 @@ function job() {
     money += moneyplus;
     updateUI();
 }
+function round_to_precision(val, decimals) {
+    const postfixes = ["", "k", "M", "B", "T", "q", "Q", "s", "S", "O", "N", 
+    "D", "Ud", "Dd", "Td", "qd", "Qd", "sd", "Sd", "Od", "Nd", 
+    "V", "Uv", "Dv", "Tv", "qv", "Qv", "sv", "Sv", "Ov", "Nv"];    
+    const components = val.toExponential(decimals).split("e");
+    const roundValTo = (val, roundTo) => { return Math.floor(val/roundTo)*roundTo };
+    const getPostFix = (exp) => { return postfixes[Math.floor(exp/3)] };
+    let [str, exp] = components;
+
+    exp = parseInt(exp);
+
+    str = (val / Math.pow(10, roundValTo(Math.max(0, exp), 3))).toFixed(decimals) + getPostFix(Math.max(0, exp));
+
+    return str;
+}
 window.onload = update;
